651062877f82bbea34d31f9f8b178c668eec8311
Matt Pitner <mattpitner09@gmail.com>
2019-08-12 19:56:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,7 @@ def speak_to_grandma(speak)
     return "I LOVE YOU TOO PUMPKIN!"
    
   
-  elsif (speak == "string".downcase)
+  elsif (speak == string.downcase)
   
     return "HUH?! SPEAK UP, SONNY!"
   end
