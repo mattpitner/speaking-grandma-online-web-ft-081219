331d38b9708cd291181a8fbe50ef3039efaf1a52
Matt Pitner <mattpitner09@gmail.com>
2019-08-12 19:48:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(speak)
-  if speak == "I LOVE YOU GRANDMA!"
+  if (speak == "I LOVE YOU GRANDMA!")
     puts "I LOVE YOU TOO PUMPKIN!"
   
 end
